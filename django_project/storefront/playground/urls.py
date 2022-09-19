ddad645feb7500192e423de0249032597ca62b3a
Tomasz <101140101+zaspanyy@users.noreply.github.com>
2022-09-19 19:59:27 +0200

    Delete django_project/storefront directory

--- django_project/storefront/playground/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('playground/hello', views.say_hello)
-]
--- django_project/storefront/playground/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def say_hello(request):
-    #pull data from db
-    #transform data or smth
-    #send emails?
-    return HttpResponse('Hello World!')
